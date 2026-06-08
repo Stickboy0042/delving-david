b65b4361ef02b8c07396ec5d955b0a472f39ace6
Stickboy0042 <d.helberg@mac.com>
2026-06-08 13:34:03 -0700

    Publish GitHub Pages HTML build from source b6fb7699

--- html/_old/repo-cleanup-20260602-183620/Create Windows Desktop Shortcut.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0launcher\windows\Create Desktop Shortcut.ps1"
-pause
--- html/_old/repo-cleanup-20260602-183620/Launch Delving David (Windows).cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0launcher\windows\launch_delving_david.ps1"
